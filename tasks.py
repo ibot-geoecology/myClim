fdd6ea67cca8e2938bbbc732208d7c601003ca9c
Vojtěch Kalčík <vojta@kalcik.cz>
2024-01-03 20:38:50 +0100

    tasks for auto building

--- tasks.py
+++ tasks.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from invoke import task
 
+def generate_documentation(c):
+    c.run(f"R -e 'devtools::document()'")
 
 @task
 def build_dev(c):
@@ -14,7 +16,12 @@ def generate(c):
     for data_file in Path("data-raw").glob("mc_data_*.R"):
         c.run(f"Rscript {str(data_file)}")
     Path("NAMESPACE").unlink(missing_ok=True)
-    c.run(f"R -e 'devtools::document()'")
+    generate_documentation(c)
+
+@task
+def generate_html(c):
+    generate_documentation(c)
+    c.run("""R -e 'pkgdown::build_site(override = list(destination = "../docs"))'""")
 
 @task
 def check(c):
@@ -23,6 +30,25 @@ def check(c):
 @task
 def check_dev(c):
     c.run("""cd .. && RD CMD check --as-cran myClim_*.*.*.tar.gz""", pty=True)
+
+@task
+def build(c):
+    """
+    build myClim package
+    """
+    c.run("""R -e 'devtools::build(".", path="../myClim_latest.tar.gz")'""")
+
+@task
+def install(c, vignette=True):
+    """
+    install myClim
+    vignette - generating vignettes default True
+    """
+    if vignette:
+        build(c)
+        c.run("""R -e 'install.packages("../myClim_latest.tar.gz", repos=NULL, build_vignettes=TRUE)'""")
+    else:
+        c.run("""R - e 'install.packages(".", repos = NULL)'""")
 
 @task
 def install(c, vignette=True):
